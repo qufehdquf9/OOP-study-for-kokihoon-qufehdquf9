68e249b033ba345a9cbd0e9973ab3e47badfc934
choi youngjin <qufehdquf9@naver.com>
2018-05-08 23:00:34 +0900

    test

--- src/membermanager/Run.java
+++ src/membermanager/Run.java
@@ -3,6 +3,7 @@ package membermanager;
 public class Run {
     public static void main(String[] args) {
         MemberManger memberManger;
-        // memberManger.run();
+        //memberManger.run();
+        System.out.println("test");
     }
 }
